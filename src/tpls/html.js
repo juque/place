4ee9ab8e04dd56c7cf18fdf7c43cae171c3fd066
Juan Pablo Aqueveque <jp.aqueveque@gmail.com>
2024-11-25 22:55:39 -0300

    add tpls folder

---
+++ src/tpls/html.js
@@ -0,0 +1,8 @@
+const tpl = `<!-- block for: {}.lower -->
+<div class="mb-5">
+  <label for="{}.lower" class="block">{}</label>
+  <input type="text" name="{}.lower" id="{}.lower" class="bg-gray-50 border border-gray-300"/>
+</div>
+<!-- /{}.lower -->
+`
+export default tpl
